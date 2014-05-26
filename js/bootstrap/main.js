7967909395fe4da4d1a6993c082755a4d1dc8b36
TheLastCard <walter@ultraclearance.com>
2014-05-26 13:42:42 +0200

    del1 og del2cA

---
+++ js/bootstrap/main.js
@@ -0,0 +1,12 @@
+//@prepros-append affix.js
+//@prepros-append transition.js
+//@prepros-append tooltip.js
+//@prepros-append alert.js
+//@prepros-append button.js
+//@prepros-append carousel.js
+//@prepros-append collapse.js
+//@prepros-append dropdown.js
+//@prepros-append modal.js
+//@prepros-append popover.js
+//@prepros-append scrollspy.js
+//@prepros-append tab.js
